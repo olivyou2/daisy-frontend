ffff6a3105390d44102b447b5a49a5d821433af1
박원호 <bag-wonho@bag-wonhoui-MacBookPro.local>
2023-03-06 18:16:45 +0900

    refactor: use Request.buildUri

--- lib/util/request.dart
+++ lib/util/request.dart
@@ -4,6 +4,12 @@ class Request {
   static String host = "http://localhost:3000";
   static Map<String, String> headers = {};
 
+  static buildUri(String url) {
+    final builtUrl = host = url;
+
+    return builtUrl;
+  }
+
   static buildHeader(Map<String, String>? headers) {
     Map<String, String> builtHeaders = {};
     builtHeaders.addAll(Request.headers);
@@ -16,14 +22,16 @@ class Request {
   }
 
   static get(String url, {Map<String, String>? headers}) {
-    var builtHeaders = Request.buildHeader(headers);
+    final builtHeaders = Request.buildHeader(headers);
+    final uri = Uri.parse(buildUri(url));
 
-    return http.get(Uri.parse(host + url), headers: builtHeaders);
+    return http.get(uri, headers: builtHeaders);
   }
 
   static post(String url, {Object? body, Map<String, String>? headers}) {
-    var builtHeaders = Request.buildHeader(headers);
+    final builtHeaders = Request.buildHeader(headers);
+    final uri = Uri.parse(buildUri(url));
 
-    return http.post(Uri.parse(host + url), headers: builtHeaders, body: body);
+    return http.post(uri, headers: builtHeaders, body: body);
   }
 }
