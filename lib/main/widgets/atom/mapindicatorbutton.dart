7a371457c04ea591ca95f73d029ca7def1e7ef8d
박원호 <wonho@bag-wonhoui-Macmini.local>
2023-03-21 16:30:00 +0900

    feat: add mapIndicatorButton widget

--- lib/main/widgets/atom/mapindicatorbutton.dart
+++ lib/main/widgets/atom/mapindicatorbutton.dart
@@ -5,6 +5,10 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 class MapIndictaorButton extends StatefulWidget {
   final String buttonName;
 
+  final Color clickedColor = ColorPalette.yellow;
+  final Color clickedBorderColor = ColorPalette.backGray;
+  final Color clickedTextColor = ColorPalette.white;
+
   const MapIndictaorButton({super.key, required this.buttonName});
 
   @override
@@ -12,25 +16,38 @@ class MapIndictaorButton extends StatefulWidget {
 }
 
 class _MapIndictaorButtonState extends State<MapIndictaorButton> {
+  bool clicked = false;
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 87.w,
-      height: 36.h,
-      decoration: BoxDecoration(
-          color: ColorPalette.white,
-          border: Border.all(color: ColorPalette.gray2),
-          borderRadius: BorderRadius.all(Radius.circular(20.w))),
-      child: Center(
-        child: Text(
-          widget.buttonName,
-          textAlign: TextAlign.center,
-          style: TextStyle(
-              fontWeight: FontWeight.w500,
-              fontSize: 15.sp,
-              color: ColorPalette.gray),
+    return GestureDetector(
+      onTapUp: onClickButton,
+      child: Container(
+        width: 87.w,
+        height: 36.h,
+        decoration: BoxDecoration(
+            color: clicked ? widget.clickedColor : ColorPalette.white,
+            border: Border.all(
+                color:
+                    clicked ? widget.clickedBorderColor : ColorPalette.gray2),
+            borderRadius: BorderRadius.all(Radius.circular(20.w))),
+        child: Center(
+          child: Text(
+            widget.buttonName,
+            textAlign: TextAlign.center,
+            style: TextStyle(
+                fontWeight: FontWeight.w500,
+                fontSize: 15.sp,
+                color: clicked ? widget.clickedTextColor : ColorPalette.gray),
+          ),
         ),
       ),
     );
+  }
+
+  void onClickButton(details) {
+    setState(() {
+      clicked = !clicked;
+    });
   }
 }
