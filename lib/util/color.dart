5d9e5836ffc93b0e795bcd84d955cde8efac18d8
박원호 <36305517+olivyou2@users.noreply.github.com>
2023-04-09 13:09:44 +0900

    feat: 지도 컴포넌트, 메인메뉴 (#2)
    * feat: map component
    
    * chore: add images to pubspec.yaml
    
    * chore: add images to project
    
    * refactor: rename topIndicator.dart to mainTopIndicator.dart
    
    * chore: add images and colors
    
    * feat: add atom widget ProfileImage
    
    * feat: add create course button into map widget
    
    * feat: add mapIndicatorButton widget
    
    * feat: mainTopIndicator
    
    * feat: main screen
    
    * feat: map
    
    * feat: map icon
    
    * feat: 뭔가엄청많은걸했는데커밋타이밍을놓쳐서한번에다함ㅠ이거스쿼시머지해요제발
    
    * chore: apply code review
    
    ---------
    
    Co-authored-by: 박원호 <wonho@bag-wonhoui-Macmini.local>

--- lib/util/color.dart
+++ lib/util/color.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
 
 class ColorPalette {
+  static const Color darkGray = Color(0xff626364);
   static const Color gray = Color(0xff8d8d8d);
   static const Color gray2 = Color(0xffb4b4b4);
   static const Color gray3 = Color(0xff626364);
+  static const Color backGray = Color(0xfff9f9fB);
 
   static const Color black = Color(0xff000000);
   static const Color white = Color(0xffffffff);
 
+  static const Color yello = Color(0xffffd12d);
+  static const Color yello2 = Color(0xfff2a309);
   static const Color yellow = Color(0xfff2a309);
 }
